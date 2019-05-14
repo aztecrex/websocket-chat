83e5e6fe9fbfe900594418d58d92c8bd01013024
Greg Wiley <gwiley@cj.com>
2019-05-14 14:22:34 -0700

    track connect and disconnect

--- handler.js
+++ handler.js
@@ -1,6 +1,35 @@
 'use strict';
 const util = require('util');
 const AWS = require('aws-sdk');
+const {promisify} = require("es6-promisify");
+
+//const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({apiVersion: '2029', endpoint: url});
+const dynamo = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+
+const putItem = p => {
+  return new Promise(
+    (resolve, reject) => {
+      dynamo.putItem(p, (err,data) => {
+        if (err) {reject (err);}
+        else {resolve(data);}
+      });
+    }
+  );
+};
+
+const deleteItem = p => {
+  return new Promise(
+    (resolve, reject) => {
+      dynamo.deleteItem(p, (err,data) => {
+        if (err) {reject (err);}
+        else {resolve(data);}
+      });
+    }
+  );
+};
+
+const connections = process.env.DYNAMODB_TABLE;
+
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
@@ -9,31 +38,66 @@ module.exports.hello = async (event) => {
 
     }, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 const sendMessageToClient = (url, connectionId, payload) => new Promise((resolve, reject) => {
-  const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({apiVersion: '2029', endpoint: url});
-  apigatewaymanagementapi.postToConnection({
-    ConnectionId: connectionId, // connectionId of the receiving ws-client
-    Data: JSON.stringify(payload),
-  }, (err, data) => {
-    if (err) {
-      console.log('err is', err);
-      reject(err);
-    }
-    resolve(data);
-  });
+  resolve({});
+//  apigatewaymanagementapi.postToConnection({
+//    ConnectionId: connectionId, // connectionId of the receiving ws-client
+//    Data: JSON.stringify(payload),
+//  }, (err, data) => {
+//    if (err) {
+//      console.log('err is', err);
+//      reject(err);
+//    }
+//    resolve(data);
+//  });
 });
 
-module.exports.csock = async (event, context) => {
-  console.log("connected: ", event.requestContext.connectionId);
 
-  return {
-    statusCode: 200
-  };
+const recordConnection = async id => {
+  const params = {
+    TableName: connections,
+    Item: {
+      connectionId: { S: id },
+    },
+  }
+  return await putItem(params);
+};
+
+const forgetConnection = async id => {
+  const params = {
+    TableName: connections,
+    Key: {
+      connectionId: { S: id },
+    },
+  }
+  return await deleteItem(params);
+};
+
+module.exports.connect = async (event, context) => {
+  const connectionId = event.requestContext.connectionId;
+  try {
+    await recordConnection(connectionId);
+  } catch(err) {
+    const resp = {statusCode: 500, body: `Failed to connect: ${connectionId} ${JSON.stringify(err)}`};
+    console.log(JSON.stringify(resp));
+    return resp;
+  }
+  return {statusCode: 200, body: `connected: ${connectionId}`};
+};
+
+
+module.exports.disconnect = async (event, context) => {
+  const connectionId = event.requestContext.connectionId;
+  try {
+    await forgetConnection(connectionId);
+    return {statusCode: 200, body: `disconnected: ${connectionId}`};
+  } catch(err) {
+    const resp = {statusCode: 500, body: `Failed to disconnect: ${connectionId} ${JSON.stringify(err)}`};
+    console.log(JSON.stringify(resp));
+    return resp;
+  }
 };
 
 module.exports.msock = async (event, context) => {
@@ -51,5 +115,4 @@ module.exports.msock = async (event, context) => {
     statusCode: 200
   };
 };
-
 
